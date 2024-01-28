6dfcc477b4309685595cea71b67fceeed6150602
Mahmoud Essam <mmensheteh@gmail.com>
2024-01-28 15:12:54 +0200

    Added Authenticaion UI and Reister form but still need some work

--- Chat Blink Clone/DatingAPPSolution/client/src/app/app.component.ts
+++ Chat Blink Clone/DatingAPPSolution/client/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-
+import { AccountService } from './_services/account.service';
+import { User } from './_models/user';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,13 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'client';
-  users: any;
-  constructor(private http: HttpClient) {}
+
+  constructor(
+    private accountService: AccountService
+  ) {}
   ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: (response) => (this.users = response),
-      error: (error) => console.log(error),
-      complete: () => console.log('Request has been Completed'),
-    });
+    this.setCurrentuser();
+  }
+  
+  setCurrentuser() {
+    const userString = localStorage.getItem('user');
+    if (!userString) return;
+    const user: User = JSON.parse(userString);
+    this.accountService.setCurrentUser(user);
   }
 }
